70a752f41a208c5ee5e18a7b044a156383a0694c
Michael Höreth <michael@hoereth.info>
2022-01-17 22:49:03 +0100

    intial commit

--- Sources/LocalDateTime/LocalDateTime.swift
+++ Sources/LocalDateTime/LocalDateTime.swift
@@ -1,3 +1,13 @@
+import Foundation
+
 struct LocalDateTime {
-    var text = "Hello, World!"
+    let components: DateComponents
+    
+    init(year: Int, month: Int, dayOfMonth: Int, hour: Int, minute: Int, second: Int) {
+        components = DateComponents(calendar: Calendar.current, year: year, month: month, day: dayOfMonth, hour: hour, minute: minute, second: second)
+    }
+    
+    init() {
+        components = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: Date())
+    }
 }
